2501791b0c15c49433096f4a3c847be1fa158477
MoritzRehm <moritz.rehm@hs-furtwangen.de>
2018-04-03 20:34:09 +0200

    Aufgabe 1.2

---
+++ Aufagbe 1.1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Aufgabe1._2
+{
+    class Program
+    {
+        static string[] subjects = { "Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore" };
+        static string[] verbs = { "braut", "liebt", "studiert", "hasst", "zaubert", "zerstört" };
+        static string[] objects = { "Zaubertränke", "den Grimm", "Lupin", "Hogwards", "die Karte des Rumtreibers", "Dementoren" };
+        
+        static int l = subjects.Length;
+        static string w1;
+        static string w2;
+        static string w3;
+        static void Main(string[] args)
+        {
+            
+            string[] verse = new string[l];
+            for (int i = 0; i < l; i++)
+            {
+                GetVerse();
+                verse[i] = w1 + " " + w2 + " " + w3;
+            }
+            for (int v = 0; v < l; v++)
+            {
+                Console.WriteLine(verse[v]);
+            }
+        }
+
+        public static void GetVerse()
+        {
+            Random rnd = new Random();
+
+            int s = rnd.Next(0, l); 
+            int v = rnd.Next(0, l); 
+            int o = rnd.Next(0, l); 
+
+            while (subjects[s] == "used")
+            {
+                s = rnd.Next(0, l);
+            }
+            while (verbs[v] == "used")
+            {
+                v = rnd.Next(0, l);
+            }
+            while (objects[o] == "used")
+            {
+                o = rnd.Next(0, l);
+            }
+            w1 = subjects[s];
+            w2 = verbs[v];
+            w3 = objects[o];
+
+            subjects[s] = "used";
+            verbs[v] = "used";
+            objects[o] = "used";
+        }
+    }
+}
---
+++ Aufgabe 1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Aufgabe_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            
+            Console.WriteLine("Würfel (w),  Kugel (k) oder Oktaeder (o)?");
+            string eingabe = (Console.ReadLine());
+
+            Console.WriteLine("Durchmesser d =");
+            double d = Console.Read();
+
+            switch(eingabe){
+                case "w":
+                Console.WriteLine("Oberfläche:" + getCubeSurface(d));
+                Console.WriteLine("Volumen:" + getCubeVolume(d));
+                break;
+
+                case "k":
+                Console.WriteLine("Oberfläche:" + getSphereSurface(d));
+                Console.WriteLine("Volumen:" + getSphereVolume(d));
+                break;
+
+                case "o":
+                Console.WriteLine("Oberfläche:" + getOktaSurface(d));
+                Console.WriteLine("Volumen:" + getOktaVolume(d));
+                break;
+                
+
+            }
+
+            /*if (eingabe == "w"){
+                Console.WriteLine("Test");
+
+            }*/
+
+        }
+
+
+            public static double getCubeSurface(double d) {
+                double ow = 6*d*d;
+                return ow;
+    
+            }
+            public static double getCubeVolume(double d){
+                double vw = d*d*d;
+                return vw;
+            }
+        
+
+
+            public static double getSphereSurface(double d) {
+                double ok = 3.14 *d*d;
+                return ok; 
+            }
+            public static double getSphereVolume(double d){
+                double vk = 3.14*d*d*d/6;
+                return vk;
+            }
+
+
+            public static double getOktaSurface(double d)  {
+                double oo = 2 * Math.Sqrt(3) * d * d;
+                return oo;
+        
+            }
+            public static double getOktaVolume(double d){
+                double vo = Math.Sqrt(2)*d*d*d/3;
+                return vo;
+            }
+    }
+}
