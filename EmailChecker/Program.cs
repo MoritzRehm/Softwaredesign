871c198973aa8104135f6f0e61ba4e6f36f75c14
MoritzRehm <moritz.rehm@hs-furtwangen.de>
2018-06-17 14:59:51 +0200

    Aufgabe 9

---
+++ EmailChecker/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace EmailChecker
+{
+    class Program
+    {
+        public static bool IsEmailAdress(string emailAdress)
+        {
+            int iAt = emailAdress.IndexOf('@');
+            int iDot = emailAdress.LastIndexOf('.');
+            return (iAt > 0 && iDot > iAt);
+        }
+
+
+        static void TestIsEmailAdress(string emailAdress, bool expected)
+        {
+            bool result = IsEmailAdress(emailAdress);
+
+             if (result == expected)
+            {
+                Console.WriteLine("SUCCESS");
+            }
+            else
+            {
+                Console.WriteLine("NO SUCCESS");
+            }
+        }
+
+
+        static void Main(string[] args)
+        {   
+
+            TestIsEmailAdress("irgendwas@web.de", true);
+            
+            TestIsEmailAdress("@web.de", false);
+
+            TestIsEmailAdress("test@web.de.be", true);
+
+            TestIsEmailAdress("a@.", false);
+        }
+    }
+}
